5091abcbaf10d65efeb29226cb4d0418ace0b2ce
AirRick6574 <eruiz6574@gmail.com>
2025-11-04 09:20:58 -0800

    Recursion assignment

---
+++ RecursionProject/src/RecursionIntro.java
@@ -0,0 +1,30 @@
+
+
+public class RecursionIntro {
+	
+	//Part 1:
+	public static long eduodd(long n) {
+		return 0; //TODO implement
+	}
+	
+	//Part 2:
+	public static int fibby(int n) {
+		return 0; //TODO implement
+	}
+	
+	
+	public static void printSparseTable(int start, int end) {
+		//TODO implement
+	}
+	
+	//Part 3:
+	public static int lp2lt(int n) {
+		return 0; //TODO implement
+	}
+	
+	public static int champion(boolean[] a) {
+		return 0; //TODO implement
+	}
+	
+
+}
